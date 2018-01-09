b0d2f1e12c499837862608234c36dd2d6f766577
CAIHANYUAN <caihanyuan@wps.cn>
2018-01-09 11:34:58 +0800

    修改banner图片路径地址

--- ksloan_server/models/banner.js
+++ ksloan_server/models/banner.js
@@ -6,27 +6,40 @@ exports.getBannerData = function(req, res) {
         fs.readFile(path.join(__dirname, './bannerdata.json'), 'utf-8', function(err, data) {
             if (err) {
                 res.statusCode = 400;
-
+                res.end();
                 console.log('error to read file: bannerdata.json');
                 throw err;
             } else {
-                var resultObj = JSON.parse(data);
-                var arr = resultObj.data;
-                for (var i = 0; i < arr.length; i++) {
-                    var bannerObject = arr[i];
-                    var imgUrl = bannerObject.xxHigh_url;
-                    bannerObject.xxHigh_url = 'http://' + req.get('host') + imgUrl;
+                try {
+                    var resultObj = JSON.parse(data);
+                    var arr = resultObj.data;
+                    for (var i = 0; i < arr.length; i++) {
+                        var bannerObject = arr[i];
+
+                        for (var key in bannerObject) {
+
+                            if (key.indexOf('HighUrl') != -1) {
+                                var imgUrl = bannerObject[key];
+                                bannerObject[key] = 'http://' + req.get('host') + imgUrl;
+                            }
+                        }
+                    }
+
+                    var resultStr = JSON.stringify(resultObj);
+                    console.log('Result obj:', resultStr);
+
+                    res.writeHead(200, {
+                        'Content-Length': Buffer.byteLength(resultStr),
+                        'Content-Type': 'text/plain;charset=utf-8'
+                    });
+                    res.write(resultStr);
+                    res.end();
+
+                } catch (err) {
+                    console.log('banner error:', err);
+                    res.statusCode = 500;
+                    res.end();
                 }
-
-                var resultStr = JSON.stringify(resultObj);
-                console.log('Result obj:', resultStr);
-
-                res.writeHead(200, {
-                    'Content-Length': Buffer.byteLength(resultStr),
-                    'Content-Type': 'text/plain;charset=utf-8'
-                });
-                res.write(resultStr);
-                res.end();
             }
         });
     } catch (e) {
